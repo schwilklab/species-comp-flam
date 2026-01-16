68c87c89bf247bcf0a63b3e095cac348169f33d8
Azaj Mahmud <azmahmud@ttu.edu>
2026-01-16 16:10:59 -0600

    Updated run_all.R and added an extra column in lfmc_wp.csv
    Added extra column "updated_combination" in lfmc_wp.csv

--- scripts/run_all.R
+++ scripts/run_all.R
@@ -11,14 +11,12 @@ source("./scripts/read_data.R")
 
 # script that reads the thermocouple data logger data during burning. Produces
 # "hobos_wider"
-
 DATA_CACHE_DIR <- "./results"
 #source("./scripts/read_hobos.R")  ## Do this once
 ## After running the above once, just read saved data:
 hobos_wider <- readRDS(file.path(DATA_CACHE_DIR, "hobos_wider"))
 
 # run PCAs requires burn_trials and hobos_wider to exist:
-
 source("./scripts/flam_pca.R")
 source("./scripts/analysis.R") # analysis.R
 source("./scripts/figures.R") # figures.R
